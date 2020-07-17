6e2f3d1e308402a9d532526d1b06aa1f51242faf
rocketdonald <mrdonaldtsang@gmail.com>
2020-07-16 20:56:23 -0700

    React update

---
+++ JS/navNfooter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const footer = (
+    <div>
+        <footer>
+            <div className="border-line"></div>
+            <h6>Created by Martin Leung & Donald Tsang (2020)</h6>
+        </footer>
+    </div>
+);
+
+ReactDOM.render(footer, document.getElementById('footer'));
