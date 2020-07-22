54b75e07f5f96004a3ffd3821f2c86b83ef3c2b1
martinL0918 <martin641379@gmail.com>
2020-07-22 22:39:04 +0800

    Fixed firebase init error

--- JS/homeRender.js
+++ JS/homeRender.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const div1ele = <h1>Hello World</h1>;
-
+var firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain:  config.authDomain,
+    databaseURL:  config.databaseURL,
+    projectId:  config.projectId,
+    storageBucket:  config.storageBucket,
+    messagingSenderId:  config.messagingSenderId,
+    appId: config.appId,
+    measurementId:  config.measurementId,
+}
+firebase.initializeApp(firebaseConfig);
 
 class Hello extends React.Component{
     render(){
