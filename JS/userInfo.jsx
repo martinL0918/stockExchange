2e40fad814799b9b1b20afb9ed6c4be4c0d7c536
rocketdonald <mrdonaldtsang@gmail.com>
2020-08-10 21:20:38 +0800

    User Info Update

--- JS/userInfo.jsx
+++ JS/userInfo.jsx
@@ -73,12 +73,20 @@
         // add constructor
         constructor(props){
           super(props);
+          this.state = {
+            userName: nickName,
+          }
         }
         // Render the whole component
         render(){
-          return;
-        }
+          return(
+            <div>
+            <InfoOptions name={this.state.userName}/>
+            <InfoDetails name={this.state.userName}/>
+            </div>
+          )
       }
+    }
 
       // Info Options
       class InfoOptions extends React.Component{
@@ -86,15 +94,28 @@
         constructor(props){
           super(props);
           this.state = {
-            changeName = false,
-            changePic = false,
-            profilePic = null
+            changeName: false,
+            changePic: false,
+            profilePic: null
           }
         }
 
         // Render Info Options
         render(){
-          return;
+          return(
+                    <div class="Personal-options col-md-4 col-sm-12">
+                        <div id="profile-img" ></div>
+                        <div class="horizontal-center vertical-center center-justify-list" 
+                        style="font-size: medium;top: 20%; position: relative;">
+                            
+                                <button href="#" type="button" class="btn btn-block btn-outline-info btn-square">Change your name</button>
+                            
+                            <li id="Change-propic">
+                                <button href="#" type="button" class="btn btn-block btn-outline-light btn-square">Change your profile picture</button>
+                            </li>
+                        </div>
+                    </div>
+          );
         }
       }
 
